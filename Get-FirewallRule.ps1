b572899aabee8195383607db285b4b8f4c36186b
Alexey Vasiliev <leshavasya@gmail.com>
2016-01-29 18:58:02 +0300

    Get-FirewallRule

--- Get-FirewallRule.ps1
+++ Get-FirewallRule.ps1
@@ -10,22 +10,13 @@ Function Get-FireWallRule {
         $Grouping
     )
     $rules=(New-object –comObject HNetCfg.FwPolicy2).rules
-    If ($name)      {$rules= $rules | where-object {$_.name     –like $name}}
-    If ($direction) {$rules= $rules | where-object {$_.direction  –eq $direction}}
-    If ($Enabled)   {$rules= $rules | where-object {$_.Enabled    –eq $Enabled}}
-    If ($protocol)  {$rules= $rules | where-object {$_.protocol  -eq  $protocol}}
-    If ($Profile)   {$rules= $rules | where-object {$_.Profiles -bAND $Profile}}
-    If ($Action)    {$rules= $rules | where-object {$_.Action     -eq $Action}}
-    If ($Grouping)  {$rules= $rules | where-object {$_.Grouping -Like $Grouping}}
+    If ($Name)      {$rules= $rules | where-object {$_.name     –like "*$Name*"}}
+    If ($Direction) {$rules= $rules | where-object {$_.direction  –eq $Direction}}
+    If ($Enabled)   {$rules= $rules | where-object {$_.enabled    –eq $Enabled}}
+    If ($Protocol)  {$rules= $rules | where-object {$_.protocol  -eq  $Protocol}}
+    If ($Profile)   {$rules= $rules | where-object {$_.profiles -bAND $Profile}}
+    If ($Action)    {$rules= $rules | where-object {$_.action     -eq $Action}}
+    If ($Grouping)  {$rules= $rules | where-object {$_.grouping -like $Grouping}}
     $rules
 }
 
-Get-firewallRule -enabled $true | sort direction,applicationName,name |
-format-table -wrap -autosize -property Name, @{Label=”Action”; expression={$Fwaction[$_.action]}},
-@{label="Direction";expression={ $fwdirection[$_.direction]}},
-@{Label=”Protocol”; expression={$FwProtocols[$_.protocol]}} , localPorts,applicationname
-
-
-$rule = Get-FireWallRule
-$rule|get-member
-$rule[0]|Get-Member
